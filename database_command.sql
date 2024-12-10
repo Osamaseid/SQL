6cef2a7df8e965c2a568eaf4ef766ae507f71c3b
osama seid <osaseid1@gmail.com>
2024-12-10 23:03:15 +0300

    database commands

--- database_command.sql
+++ database_command.sql
@@ -1,16 +1,24 @@
 --connect to database
 psql - U username - d database_name ---
---create database
+--create database---
 CREATE DATABASE my_database;
 ----
-CREATE TABLE employees (
+--- delete database---
+DROP DATABASE my_database;
+---
+CREATE TABLE user (
     id INTEGER PRIMARY KEY,
     name VARCHAR(100),
     position VARCHAR(50),
     salary DECIMAL(10, 20),
     hire_date DATE
 );
---/d -to list all tables in the current database
+--/d -to list all tables in the current database--
 --insert to database table
-INSERT INTO employees (name, position, salary)
+INSERT INTO user (name, position, salary)
 VALUES('osama', 'head', '20000');
+--- reate an inde --
+CREATE INDEX idx_users_email ON users (email);
+---
+---drop an inde --
+DROP INDEX idx_users_email;
